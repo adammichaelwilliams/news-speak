1d2c097bb7d7f09754fc5ffa2b7ee38b2e00ef79
Adam Williams <kainolophobia@gmail.com>
2012-03-17 01:35:31 -0400

    fox_scrape

--- chrome_ext/js/bar.js
+++ chrome_ext/js/bar.js
@@ -9,20 +9,6 @@ $.get(foot, function(data){
     $("body").append(data);
     });
 
-chrome.extension.onConnect.addListener(function(port) {
-  console.assert(port.name == "knockknock");
-  port.onMessage.addListener(function(msg) {
-    if (msg.joke == "Knock knock")
-      port.postMessage({question: "Who's there?"});
-    else if (msg.answer == "Madame")
-      port.postMessage({question: "Madame who?"});
-    else if (msg.answer == "Madame... Bovary")
-      port.postMessage({question: "I don't get it."});
-  });
-});
-
-
-
 
 /*
 chrome.tabs.onUpdated.addListener(onTabUpdated);

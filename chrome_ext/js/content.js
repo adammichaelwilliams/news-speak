40e5cf0f598b82a84fdfa16459de9430103171b1
DiamRem <kewu@Ke-Wus-MacBook-Pro.local>
2012-03-17 08:04:37 -0400

    minimize

--- chrome_ext/js/content.js
+++ chrome_ext/js/content.js
@@ -4,7 +4,7 @@ document.body.innerHTML += [
 	, '<div id="nsp_toolbar">'
 	, '<h4 id="nsp_title" style="color: #f6f6f6 !important;">Room Title</h4>'
 	, '<span id="nsp_minimize" style="color: #f6f6f6 !important;" class="nsp_toolbar_btn">_</span>'
-	, '<span id="nsp_close" style="color: #f6f6f6 !important;" class="nsp_toolbar_btn">X</span>'
+	, '<span id="nsp_close" style="color: #f6f6f6 !important; visibility:hidden;" class="nsp_toolbar_btn">X</span>'
 	, '</div>'
 	, '<div id="nsp_conversation">'
 	, '<ul id="nsp_comment_list" style="list-style-type: none !important;">'
@@ -32,7 +32,6 @@ $("#nsp_input").height(new_height);
 
 $("#nsp_text_input").keyup(function(e) {
 	$("#nsp_input").height($("#nsp_text_input").height());
-
 	if(e.keyCode === 16) {
 		shift_state = false;	
 	} else if(e.keyCode === 13 && !shift_state) {
@@ -47,4 +46,20 @@ $("#nsp_text_input").keydown(function(e) {
 	if(e.keyCode === 16) {
 		shift_state = true;
 	}
+});
+
+var nsp_min = false;
+$("#nsp_minimize").click(function(){
+        if(!min){
+            $("#nsp_conversation").hide('slow', function(){
+                $("#nsp_minimize").text('+');
+            });
+            $("#nsp_input").hide();
+            nsp_min = true;
+        }else{
+            $("#nsp_conversation").show('slow');
+            $("#nsp_input").show();
+            $("#nsp_minimize").text('_');
+            nsp_min = false;
+        }
 });

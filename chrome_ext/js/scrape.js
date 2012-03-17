776b552b8aae4524a0702520fa6b1dbd13f7cc04
Yunxing Dai <yunxing@umich.edu>
2012-03-17 10:54:06 -0400

    polished parser

--- chrome_ext/js/scrape.js
+++ chrome_ext/js/scrape.js
@@ -1,48 +1,58 @@
 (function scrape()
-{
-    console.log("parser started");
-    var common = {};
-    var res = {};
+ {
+     console.log("parser started");
+     var common = {};
+     var res = {};
 
-    CommonWords = CommonWords.map(stemmer);
+     CommonWords = CommonWords.map(stemmer);
 
-    html = $("h1").html()
-		if(html) {
-			html = html.trim();
-			while (html[0] == "<")
-			html = html.split(">")[1];
-		} else {
-			html = window.document.title;
-		}
-        if(!html){
-			html = window.document.title;
-        }
+     html = $("h1").html()
+     try
+     {
+	 if(html) {
+	     html = html.trim();
+	     while (html[0] == "<")
+	     {
+		 html = html.split(">")[1];
+		 html = html.split("<")[0];
+	     }
+	 } else {
+	     html = window.document.title;
+	 }
+	 if(!html){
+	     html = window.document.title;
+	 }
+     }
+     catch(e)
+     {
+	 html = window.document.title;
+     }
 
-		console.log("Title:", html);
+     console.log("Title:", html);
 
-    var titles = html.split(" ");
-    titles = titles.map(stemmer);
-    titles = titles.map(function(element)
-			{
-			    return element.toLowerCase();
-			}
-		       );
-    titles = titles.map(function(element)
-			{
-			    return element.replace(/,/g,"");
-			}
-		       );    
-    titles = titles.filter(function(element)
-			   {
-			       return !(CommonWords.indexOf(element) >= 0);
-			   }
-			  );
+     var titles = html.split(" ");
+     titles = titles.map(stemmer);
+     titles = titles.map(function(element)
+			 {
+			     return element.toLowerCase();
+			 }
+			);
+     titles = titles.map(function(element)
+			 {
+			     return element.replace(/,/g,"");
+			 }
+			);    
+     titles = titles.filter(function(element)
+			    {
+				return !(CommonWords.indexOf(element) >= 0);
+			    }
+			   );
 
-    result = {}
-    result.title = html;
-    result.keywords = titles;
-    result.url = document.URL;
-    
-		store_room_data(result);
-		console.log(JSON.stringify(result));
-})();
+     result = {}
+     result.title = html;
+     result.keywords = titles;
+     result.url = document.URL;
+     
+     store_room_data(result);
+     console.log(JSON.stringify(result));
+ })();
